f0bcb203bba3f0762a09dc3290c7f5fe48c0038b
eowiz <eowiz@proton.me>
2023-01-01 02:44:53 +0900

    :tada: Init commit

---
+++ src/main/java/com/github/eowiz/kozo/KozoApplication.java
@@ -0,0 +1,13 @@
+package com.github.eowiz.kozo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KozoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KozoApplication.class, args);
+	}
+
+}
